26c94ddfb8e9e58869003249fd100555c5aaa749
iamaadityaraj <132557964+iamaadityaraj@users.noreply.github.com>
2024-02-17 20:50:44 +0530

    Add files via upload

---
+++ project.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class project {
+      public static void main(String[]args){
+     // System.out.println("hello world"); 
+
+     Scanner sc=new Scanner(System.in);
+    /*  System.out.println("Enter the fist number");
+      int a=sc.nextInt();
+      System.out.println("Enter the second number");
+      int b=sc.nextInt();
+      int addition=0;
+      addition =a+b;
+      System.out.println("the sum of number is :"+ addition);*/
+
+
+     /*  System.out.println("enter the first number");
+      int a=sc.nextInt();
+      System.out.println("enter the second number");
+      int b=sc.nextInt();
+      int substraction=0;
+      substraction=b-a;
+      System.out.println("the subtract of number is:"+substraction);*/
+
+     /*  System.out.println("enter the first number");
+      int a=sc.nextInt();
+      System.out.println("enter the second number");
+      int b=sc.nextInt();
+      int multiplication=0;
+      multiplication=a*b;
+      System.out.println("the multiplication of number is:"+multiplication);*/
+
+      System.out.println("enter the first number");
+      int a=sc.nextInt();
+      System.out.println("enter the second number");
+      int b=sc.nextInt();
+      int division=0;
+      division=a/b;
+      System.out.println("the division of number is:"+division);
+      
+
+
+
+
+      }
+}
+    
